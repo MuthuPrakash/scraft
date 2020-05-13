7e1b2c12db5b06e227cc18398d978e6bd64f5e2e
Muthu <muthu167@gmail.com>
2020-05-12 21:18:28 -0400

    Home page styles update and footer component update

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,14 @@ import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
 import Products from './pages/Products';
 import { Route, HashRouter } from 'react-router-dom';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap'
+import { Container, Row, Col, Navbar, Nav, FormControl, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './assets/logo/scraft_freesketch_200x50.png'; // with import
+import instagram_icon_footer from './assets/banner/instagram_icon_footer.jpg';
+import facebook_icon_footer from './assets/banner/facebook_icon_footer.jpg';
+import email_subscribe_icon_footer from './assets/banner/email_subscribe_icon_footer.png';
+import phone_icon_footer from './assets/banner/phone_icon_footer.png';
+import whatsapp_icon_footer from './assets/banner/whatsapp_icon_footer.png';
 
 export default class Main extends Component {
     constructor(props) {
@@ -44,6 +49,71 @@ export default class Main extends Component {
                         <Route exact path="/" component={Home} />
                         <Route path="/products" component={Products} />
                         <Route path="/contact" component={ContactUs} />
+                    </Row>
+
+                    {/* Footer Information */}
+                    <Row>
+                        <Row>
+                            <Col lg={12} xl={12}>
+                                <div className='footerSeparator'></div>
+                            </Col>
+                        </Row>
+                        <Row className='footerRow'>
+                            <Col lg={2} className='webInfo'>
+                                <div className='webInfoTitle'>
+                                    <span>SCraft</span>
+                                </div>
+                                <Nav>
+                                    <Nav.Link href="#/">HOME</Nav.Link>
+                                    <Nav.Link href="#/products">INVITATION</Nav.Link>
+                                    <Nav.Link href="#/products">GIFTS</Nav.Link>
+                                    <Nav.Link href="#/products">BACKDROPS</Nav.Link>
+                                    <Nav.Link href="#/about">ABOUT</Nav.Link>
+                                    <Nav.Link href="#/contact">CONTACT</Nav.Link>
+                                </Nav>
+                            </Col>
+                            <Col lg={3} className='helpInfo'>
+                                <div className='helpInfoTitle'>
+                                    <span>Help</span>
+                                </div>
+                                <Nav>
+                                    <Nav.Link href="#/">SHIPPING AND RETURNS</Nav.Link>
+                                    <Nav.Link href="#/products">STORE POLICY</Nav.Link>
+                                    <Nav.Link href="#/products">PAYMENT METHODS</Nav.Link>
+                                    <Nav.Link href="#/products">FAQ</Nav.Link>
+                                </Nav>
+                            </Col>
+                            <Col lg={3} className='contactInfo'>
+                                <div className='contactInfoTitle'>
+                                    <span>Contact</span>
+                                </div>
+                                <Nav>
+                                    <Nav.Link href="tel:1234567890">1234567890 <img src={phone_icon_footer} alt='' /></Nav.Link>
+                                    <Nav.Link href="mailto:info@scraft.com">INFO@SCRAFT.COM <img src={email_subscribe_icon_footer} alt='' /></Nav.Link>
+                                    <Nav.Link href="https://instagram.com/scraft">Instagram <img src={instagram_icon_footer} alt='' /></Nav.Link>
+                                    <Nav.Link href="https://facebook.com/scraft">Facebook <img src={facebook_icon_footer} alt='' /></Nav.Link>
+                                    <Nav.Link href="tel:1234567890">Whatsapp <img src={whatsapp_icon_footer} alt='' /></Nav.Link>
+                                </Nav>
+                            </Col>
+                            <Col lg={4} className='newsletterInfo'>
+                                <div className='newsLetterInfoTitle'>
+                                    <span>Newsletter</span>
+                                </div>
+                                <div className='newsLetterComponents'>
+                                    <div>
+                                        <span>
+                                            Enter Email*
+                                    </span>
+                                    </div>
+                                    <div>
+                                        <FormControl aria-describedby="basic-addon1" placeholder="Email Address"/>
+                                    </div>
+                                    <div className='newsLetterButtonDiv'>
+                                        <Button className='newsLetterButton' variant="dark">SUBSCRIBE</Button>{' '}
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
                     </Row>
                 </Container>
             </HashRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import home_banner from './../assets/banner/home_banner.jpg'; // with import
+import ourFavorites_banner from './../assets/banner/ourFavorites_banner.jpg'; // with import
+import paintings_banner from './../assets/banner/paintings_banner.jpg';
+import props_banner from './../assets/banner/props_banner.jpg';
 import { Button } from 'react-bootstrap'
 
 export default class Home extends Component {
@@ -22,6 +25,71 @@ export default class Home extends Component {
                         </div>
                     </Col>
                 </Row>
+                <Row>
+                    <Row>
+                        <Col lg={12}>
+                            <div className='ourFavoritesHeader'>
+                                <span>OUR FAVORITES</span>
+                            </div>
+                        </Col>
+                    </Row>
+
+{/* Collection 1 Information */}
+                    <Row className='weddingInvRow'>
+                        <Col lg={6} className='weddingInvColl'>
+                            <div className='weddingInvCollTitle'>
+                                <span>WEDDING INVITATIONS</span>
+                            </div>
+                            <div className='weddingInvCollSubTitle'>
+                                <span>shop various collection of wedding invitations here</span>
+                            </div>
+                            <div>
+                                <div className='weddingInvCollButtonDiv'><Button className='weddingInvCollButton' variant="dark">Shop Invitatios</Button>{' '}</div>
+                            </div>
+                        </Col>
+                        <Col lg={6}>
+                            <div className='weddingInvCollBanner'><img className='weddingInvCollBannerImage' src={ourFavorites_banner} alt='Wedding Invitation collection'></img></div>
+                        </Col>
+                    </Row>
+
+{/* Collection 2 Information */}
+                    <Row className='giftPaintingsRow'>
+                        <Col lg={6}>
+                            <div className='giftPaintingsCollBanner'><img className='giftPaintingsCollBannerImage' src={paintings_banner} alt='Wedding Invitation collection'></img></div>
+                        </Col>
+
+                        <Col lg={6} className='giftPaintingsColl'>
+                            <div className='giftPaintingsCollTitle'>
+                                <span>GIFT PAINTINGS</span>
+                            </div>
+                            <div className='giftPaintingsCollSubTitle'>
+                                <span>shop various collection of Gift Paintings here</span>
+                            </div>
+                            <div>
+                                <div className='giftPaintingsCollButtonDiv'><Button className='giftPaintingsCollButton' variant="dark">Shop Gifts</Button>{' '}</div>
+                            </div>
+                        </Col>
+                    </Row>
+
+{/* Collection 3 Information */}
+                    <Row className='weddingInvRow'>
+                        <Col lg={6} className='weddingInvColl'>
+                            <div className='weddingInvCollTitle'>
+                                <span>BACKDROPS & PROPS</span>
+                            </div>
+                            <div className='weddingInvCollSubTitle'>
+                                <span>shop various collection of backdrops and props here</span>
+                            </div>
+                            <div>
+                                <div className='weddingInvCollButtonDiv'><Button className='weddingInvCollButton' variant="dark">Shop Backdrops</Button>{' '}</div>
+                            </div>
+                        </Col>
+                        <Col lg={6}>
+                            <div className='weddingInvCollBanner'><img className='weddingInvCollBannerImage' src={props_banner} alt='Wedding Invitation collection'></img></div>
+                        </Col>
+                    </Row>
+                </Row>
+
             </Row>
         )
     }
